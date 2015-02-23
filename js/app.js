091ba4810f84e07439064313e6dbaa99dc6b3620
Alex Kras <alex.kras@support.com>
2015-02-23 09:05:08 -0800

    Cleaned up app.js to supported functionality

--- js/app.js
+++ js/app.js
@@ -6,9 +6,4 @@ function show(boardName){
     console.log(boardName.toString());
 }
 
-board.randomize();
-//while(!board.checkCell(0,0)){
-//    board.randomize();
-//}
 show(board);
-console.log(board.checkCell(0,0));
