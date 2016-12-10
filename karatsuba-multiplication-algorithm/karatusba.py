f74a0dead149ebb23695338b7b7aab1258ed9535
Alex Kras <akras14@gmail.com>
2016-12-10 14:03:46 -0800

    Add unit tests

--- karatsuba-multiplication-algorithm/karatusba.py
+++ karatsuba-multiplication-algorithm/karatusba.py
@@ -22,23 +22,3 @@ def mult(l,r):
     result = ac*10**(2*middle) + ad_plus_bc*10**middle + bd
     return result
 
-def test(l,r):
-    print l
-    print r
-    print mult(l,r)
-    print l*r
-    print("\n")
-
-#assert(mult(5,0) == 0)
-#assert(mult(5,5) == 25)
-#assert(mult(12,34) == 408)
-#test(2222,3333)
-#test(22222,33333)
-#test(222222,333333)
-#test(2222222,3333333)
-first = 3141592653589793238462643383279502884197169399375105820974944592
-second = 2718281828459045235360287471352662497757247093699959574966967627
-
-print mult(first, second)
-#print first*second
-#assert(mult(123456,75643) == 9338582208)
