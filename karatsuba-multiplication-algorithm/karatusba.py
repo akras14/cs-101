af3771ec64f62ab1eef2824c23534f904d14878b
Alex Kras <akras14@gmail.com>
2016-12-09 21:33:33 -0800

    Working, need to clean up tests

--- karatsuba-multiplication-algorithm/karatusba.py
+++ karatsuba-multiplication-algorithm/karatusba.py
@@ -1,7 +1,6 @@
 def mult(l,r):
-    print "in mult " + str(l) + " " + str(r)
     if l < 10 or r < 10:
-        return long(l)*long(r)
+        return l*r
     
     # Find middle point
     lSize = len(str(l))
@@ -9,20 +8,37 @@ def mult(l,r):
     longest = max(lSize, rSize);
     middle = longest/2
     
-    # TODO Check lecture notes for pseudo code
     # split numbers in the middle
-    #lHigh = l/10**middle
-    #lLow = l%10**middle
-    #rHigh = r/10**middle
-    #lLow = r%10**middle
+    power = 10**middle
+    a = l/power
+    b = l%power
+    c = r/power
+    d = r%power
 
     # Do Karatsuba magic
-    #z0 = mult(lLow,rLow)
-    #z1 = mult((lLow+lHigh), (rLow+rHigh))
-    #z2 = mult(lHigh, rHight)
-    #return z2*10**(2*middle) + (z1-z2-z0)*10**(middle) + z0
+    ac = mult(a,c)
+    bd = mult(b,d)
+    ad_plus_bc = mult(a+b, c+d) - ac - bd
+    result = ac*10**(2*middle) + ad_plus_bc*10**middle + bd
+    return result
 
-assert(mult(5,0) == 0)
-assert(mult(5,5) == 25)
-assert(mult(22,33) == 726)
+def test(l,r):
+    print l
+    print r
+    print mult(l,r)
+    print l*r
+    print("\n")
+
+#assert(mult(5,0) == 0)
+#assert(mult(5,5) == 25)
+#assert(mult(12,34) == 408)
+#test(2222,3333)
+#test(22222,33333)
+#test(222222,333333)
+#test(2222222,3333333)
+first = 3141592653589793238462643383279502884197169399375105820974944592
+second = 2718281828459045235360287471352662497757247093699959574966967627
+
+print mult(first, second)
+#print first*second
 #assert(mult(123456,75643) == 9338582208)
