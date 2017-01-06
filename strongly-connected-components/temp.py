6cb3a9bc80de18ceb945edd0a0fe6f534ee75e8b
Alex Kras <akras14@gmail.com>
2017-01-06 10:22:07 -0800

    Updated sample data to match example
    Calculates valid finish time

--- strongly-connected-components/temp.py
+++ strongly-connected-components/temp.py
@@ -1,13 +1,13 @@
 data = {
-    1: [7],
-    2: [5],
-    3: [9],
-    4: [1],
-    5: [8],
-    6: [3, 8],
-    7: [4, 9],
-    8: [2],
-    9: [6],
+    1: [4],
+    2: [8],
+    3: [6],
+    4: [7],
+    5: [2],
+    6: [9],
+    7: [1],
+    8: [5, 6],
+    9: [3, 7]
 }
 
 def reverseGraph(graph):
@@ -20,23 +20,30 @@ def reverseGraph(graph):
                 newGraph[edge].append(k)
     return newGraph
 
+
 t = 0 # Number of nodes process so far
 s = None # Current source node
-
+finish_time = {}
+leaders = {}
+visited = []
 def dfsLoop(graph):
-    visited = []
-    for i in range(len(data), 0, -1):
+    global s
+    for i in range(len(data), 0, -1): # All nodes in reverse order
         if i not in visited:
-            leader = i
+            s = i
             dfs(graph, i)
 
-#revData = reverseGraph(data)
-
 def dfs(graph, node):
-    visited = []
-    next = [node]
-    while len(next) > 0:
-        node = next.pop()
-        if node not in visited:
-            visited.append(node)
-            next = next + graph[node]
+    global t
+    if node not in visited:
+        visited.append(node)
+        leaders[node] = s
+    for next in graph[node]:
+        if next not in visited:
+            dfs(graph, next)
+    t += 1
+    finish_time[node] = t
+
+revData = reverseGraph(data)
+dfsLoop(revData)
+print finish_time
