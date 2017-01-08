47a555c6975c6659d6f61fc67ce29aa6a2b4074f
Alex Kras <akras14@gmail.com>
2017-01-08 14:08:05 -0800

    Tryied to load data from file

--- strongly-connected-components/large-test.py
+++ strongly-connected-components/large-test.py
@@ -1,9 +1,12 @@
-data = []
+data = {}
 with open("data.txt") as f:
   for line in f:
     u, v = line.split()
     u = int(u)
     v = int(v)
-    data.append((u,v))
+    if u in data:
+        data[u].append(v)
+    else:
+        data[u] = [v]
 
 print len(data)
