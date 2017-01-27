dcc78ec1dd087def48232027718cd30e57759a3a
Alex Kras <akras14@gmail.com>
2017-01-26 22:59:47 -0800

    Add minor refactoring

--- dijkstra-shortest-path/di.py
+++ dijkstra-shortest-path/di.py
@@ -3,16 +3,17 @@
 
 def di(graph, node):
     """Compute Dijkstra shortest path"""
+    MAX_DIST = 10000
     visited = {}
     dist = {}
     all_nodes = graph.get_nodes()
     for n in all_nodes:
-        dist[n] = 10000
+        dist[n] = MAX_DIST
         visited[n] = False
     v = node
     dist[v] = 0
 
-    while visited[v] == False:
+    while visited[v] is False:
         visited[v] = True
         edges = graph.get_edges(v)
 
@@ -21,9 +22,9 @@ def di(graph, node):
             if dist[w] > (dist[v]+weight):
                 dist[w] = dist[v] + weight
         v = 1
-        minDist = 10000
+        minDist = MAX_DIST
         for i in range(1, graph.size() + 1):
-            if visited[i] == False and minDist > dist[i]:
+            if visited[i] is False and minDist > dist[i]:
                 minDist = dist[i]
                 v = i
     return dist
